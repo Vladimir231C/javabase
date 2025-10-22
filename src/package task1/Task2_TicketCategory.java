6ac606a681f83cd2c0164707bfc26b94b792d051
Vladimir231C <Podsekalovv@bk.ru>
2025-10-22 13:32:57 +0300

    Update Task2_TicketCategory.java

--- src/package task1/Task2_TicketCategory.java
+++ src/package task1/Task2_TicketCategory.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 
-public class Task2_TicketCategory {
+public class TicketCategory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите возраст: ");
+        
+        System.out.print("Введите возраст посетителя: ");
         int age = scanner.nextInt();
-
+        
         String category;
+        
         if (age < 7) {
             category = "детский";
         } else if (age >= 7 && age <= 17) {
             category = "подросток";
         } else if (age >= 18 && age <= 60) {
             category = "взрослый";
-        } else {
+        } else if (age >= 61) {
             category = "пенсионный";
+        } else {
+            category = "неопределенная категория";
         }
-
+        
         System.out.println("Категория билета: " + category);
+        
         scanner.close();
     }
 }
